64cd1f8687b395fb51e5eaf1cef08312d82133ee
Peter Szinek <peter@rubyrailways.com>
2008-12-06 17:45:21 +0100

    temporarily dropped exporting; replaced with explanation and tree showing generated XPaths

--- lib/scrubyt/output/scrubyt_result.rb
+++ lib/scrubyt/output/scrubyt_result.rb
@@ -2,36 +2,41 @@ module Scrubyt
   class ScrubytResult < ResultNode
     attr_accessor :root_patterns, :source_file, :source_proc
 
-    def export(arg1, output_file_name=nil, extractor_result_file_name=nil)
-      if arg1.is_a? String
-        if File.exists? arg1
-          export_old1(arg1, output_file_name, extractor_result_file_name)
-        else
-          export_old2(arg1, output_file_name, extractor_result_file_name)
-        end
-      else
-        export_new(arg1)
-      end
-    end
-
-    def show_stats
-      #Implement me...
-    end
-
-    def export_old1(input_file, output_file_name=nil, extractor_result_file_name=nil)
-      contents = open(input_file).read
-      wrapper_name = contents.scan(/\s+(.+)\s+=.*Extractor\.define.*/)[0][0]
-      export_old2(wrapper_name, output_file_name, extractor_result_file_name)
-    end
-
-    def export_old2(wrapper_name, output_file_name=nil, extractor_result_file_name=nil)
-      export_new({ :wrapper_name => wrapper_name, :output_file_name => output_file_name || "#{wrapper_name}_extractor_export.rb", :extractor_result_file_name => extractor_result_file_name })
-    end
-    
-    def export_new(data)
-      data[:source_file] = @source_file
-      data[:source_proc] = @source_proc
-      Scrubyt::Export.export(@root_patterns, data)
+    def export
+      #Temporary solution; the real one will be back later - or not     
+     result = <<-EXPLANATION
+     
+     === Extractor tree ===
+     
+     export() is not working at the moment, due to the removal or ParseTree, ruby2ruby and RubyInline.
+     For now, in case you are using examples, you can replace them by hand based on the output below.
+     So if your pattern in the learning extractor looks like
+     
+     book "Ruby Cookbook" 
+     
+     and you see the following below:
+     
+     [book] /table[1]/tr/td[2]
+     
+     then replace "Ruby Cookbook" with "/table[1]/tr/td[2]" (and all the other XPaths) and you are ready!
+     
+     EXPLANATION
+     
+     tree_builder = lambda do |node, level|        
+       result += current_level = ("   " * (level == 0 ? 0 : level-1) + 
+                                  "|\n" * (level == 0 ? 0 : 1) +
+                                  "   " * (level == 0 ? 0 : level-1) + 
+                                 "+-- " * (level == 0 ? 0 : 1) + 
+                                 "[#{node.name}]")
+       result += " #{node.filters[0].xpath}" if node.type == :tree
+       result += "\n"                                 
+                                 
+       node.children.each {|c| tree_builder[c, level+1]}
+     end
+     
+     tree_builder[root_patterns[0],0]
+          
+     result += "\n"
     end
   end
 end
