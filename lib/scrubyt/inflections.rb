0061b5a718851257bbdd0b2f8c721595cc0d9279
Maury Guillaume <hiorin@paw.local>
2009-03-19 21:00:50 +0800

    currently inflections are not working correctly in scrubyt (camelize calls Inflector.singularize which doesn't exist and the unless check if the object responds to the function doesn't work because it's checking against the class instead of the instance)... So instead of reinventing the wheel why not just require extlib?

--- lib/scrubyt/inflections.rb
+++ lib/scrubyt/inflections.rb
@@ -1,30 +1,17 @@
-require "#{File.dirname(__FILE__)}/inflector.rb"
-module Scrubyt
-  module CoreExtensions
-    module String
-      module Inflections
-        def self.included(klass)
-          unless klass.respond_to?(:camelize) || klass.respond_to?(:classify) || klass.respond_to?(:constantize)
-            klass.class_eval do 
-              def camelize
-                Scrubyt::Inflector.camelize(self, true)
-              end
-
-              def classify
-                Scrubyt::Inflector.classify(self)
-              end
-
-              def constantize
-                Scrubyt::Inflector.constantize(self)
-              end
-            end
-          end
-        end              
-      end
+unless "".respond_to?(:camelize) && "".respond_to?(:classify) && "".respond_to?(:constantize)
+  require 'extlib'
+  
+  class String
+    def classify
+      Extlib::Inflection.classify(self)
     end
-  end  
-end
-
-class String
-  include Scrubyt::CoreExtensions::String::Inflections
+    
+    def camelize
+      Extlib::Inflection.camelize(self)
+    end
+    
+    def constantize
+      Extlib::Inflection.constantize(self)      
+    end
+  end
 end
